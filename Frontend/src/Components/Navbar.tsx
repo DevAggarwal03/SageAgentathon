3b21cd4a09bac3e09ccffac7678a962bfc1f5810
Dev Aggarwal <devaggarwal1009@gmail.com>
2025-02-14 18:58:02 +0530

    changed logo

--- Frontend/src/Components/Navbar.tsx
+++ Frontend/src/Components/Navbar.tsx
@@ -1,12 +1,15 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { Link, NavLink } from "react-router-dom";
-
+import logo from "../assets/noBgLogo.png";
 
 const Navbar = () => {
     return (
         <div className="bg-none">
             <nav className="border-b border-white bg-none p-4 flex justify-between items-center">
-                <Link to="/" className="text-white text-3xl font-bold">DEX</Link>
+            <Link to="/" className="text-white text-3xl font-bold flex items-center gap-x-2">
+                    <img src={logo} alt="logo" className="  w-14" />
+                    <span className="text-white text-xl font-bold">Agentic Dex</span>
+                </Link>
                 <div className="flex gap-x-4 text-white text-xl">
                     <NavLink to="/swap" className={({ isActive }) => isActive ? "text-blue-500" : ""}><span>Swap</span></NavLink>
                     <NavLink to="/pools" className={({ isActive }) => isActive ? "text-blue-500" : ""}><span>Pool</span></NavLink>
